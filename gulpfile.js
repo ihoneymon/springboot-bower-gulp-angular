a2b66300cb01d8475a3fe0daa1a531ccc3e8dfbd
honeymon <ihoneymon@gmail.com>
2016-05-01 17:40:35 +0900

    index.html 추가 및 설정정보 일부 변경

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp = require('gulp');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('default', function() {
   // place code for your default task here
